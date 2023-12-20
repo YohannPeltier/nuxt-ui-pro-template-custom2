fcdb4b7f3f3c2bfafa0053876c560233bc91d1e9
YohannPeltier <yohannpeltier@gmail.com>
2023-12-21 00:58:10 +0100

    d

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,16 @@ export default defineNuxtConfig({
 			"~/components",
 		],
 	},
+	hooks: {
+		// Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+		"components:extend": (components) => {
+			const globals = components.filter((c) =>
+				["UButton"].includes(c.pascalName)
+			)
+
+			globals.forEach((c) => (c.global = true))
+		},
+	},
 	ui: {
 		icons: ["heroicons", "simple-icons"],
 	},
